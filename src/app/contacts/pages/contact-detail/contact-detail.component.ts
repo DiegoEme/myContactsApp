73838aa9f80fde82673ee0ddc8fdc007c3c9148e
unknown <soterg.uhdeia@gmail.com>
2021-10-21 20:17:35 -0500

    dynamic routerLink and getting data from url using activatedRoute

--- src/app/contacts/pages/contact-detail/contact-detail.component.ts
+++ src/app/contacts/pages/contact-detail/contact-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-contact-detail',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => console.log('los params',params))
   }
 
 }
